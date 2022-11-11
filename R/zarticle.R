8b708954d48f93c1f9846061243b4a04510747a3
isaiahespi <gespinoz@umd.edu>
2022-11-10 22:03:43 -0500

    format creation attempt

---
+++ R/zarticle.R
@@ -0,0 +1,23 @@
+#' Article template
+#'
+#' A template for academic articles. Custom made by Isaiah somehow
+#'
+#' @param ... Arguments to [`rmarkdown::pdf_document`].
+#'
+#' @return A custom article template
+#' @export
+#'
+#'
+zarticle <- function(...) {
+  # locations of resource files in the package
+  pkg_resource = function(...) {
+    system.file(..., package = "zealot")
+  }
+
+  zarticle <- pkg_resource("resources/zarticle-template.tex")
+
+  # call the base pdf_document function
+  rmarkdown::pdf_document(
+    template = zarticle,
+    ...)
+}
