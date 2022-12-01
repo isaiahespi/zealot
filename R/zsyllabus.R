70732eca95cfa775e7a7df78f46713c013fc2750
isaiahespi <gespinoz@umd.edu>
2022-12-01 02:23:00 -0500

    I just want the damn logo

--- R/zsyllabus.R
+++ R/zsyllabus.R
@@ -1,17 +1,20 @@
-#' My Syllabus template
+#' My UMD Syllabus template
 #'
-#' @description My description for this I guess
+#' @description A template to construct a syllabus formatted to follow the
+#' University of Maryland Accessible Syllabus template. I tried my best to
+#' make this template identical to the document shared by the Teaching and
+#' Learning Transformation Center.
 #'
-#'#' # YAML header fields
+#' YAML header fields
 #'
 #' | FIELD  | DESCRIPTION |
 #' | ------ | ----------- |
-#' | `title` | Title of the class (course number) |
+#' | `title` | Title of the class |
+#' | `subtitle`| (course number)|
 #' | `term` | Season/Year |
 #' | `author` | Name(s) of the professor(s) |
 #' | `date` | start and end dates the course |
 #' | `email` | Your email. |
-#' | `web` | Class website.|
 #' | `officehours` | Your office hours.|
 #' | `office` | Your office location. |
 #' | `classroom` | The classroom location.|
@@ -21,27 +24,21 @@
 #'
 #'
 #'
-#' @param ... Arguments to [`rmarkdown::pdf_document`].
+#' @param ...
+#' Arguments to [`rmarkdown::pdf_document()`].
+#'
+#' @return An R Markdown format object
 #'
 #' @md
 #' @export
 #'
-zsyllabus <- function(...) {
-  # locations of resource files in the package
-  pkg_resource = function(...) {
-    # locations of resource files in the package
-    system.file("rmarkdown", "templates", "z-syllabus", "resources",
-                "zsyllabus-template.tex",
-                package = "zealot")
-  }
-
-  zsyllabus = pkg_resource("resources/zsyllabus-template.tex")
-  logo = pkg_resource("resources/umd_logo.png")
-
+zsyllabus <- function(...){
+  zsyllabus <- system.file("rmarkdown", "templates", "z-syllabus", "resources",
+                           "zsyllabus-template.tex",
+                           package = "zealot")
   # call the base pdf_document function
   rmarkdown::pdf_document(
     template = zsyllabus,
-    keep_tex = TRUE,
-    logo = logo,
     ...)
+
 }
